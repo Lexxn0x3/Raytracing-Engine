14f48cce821444c41fafa9e156e5aada2510041d
Lexxn0x3 <35461915+Lexxn0x3@users.noreply.github.com>
2020-11-28 21:03:23 +0100

    1

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Numerics;
+using System.Windows;
 
 namespace Raytracing_Csharp
 {
@@ -18,17 +19,42 @@ namespace Raytracing_Csharp
             int width =    1920;
             int height =   1080;
             int colorDepth = 255;
+            float fov = (float)(Math.PI/3);
+            Sphere sphere = new Sphere();
+            sphere.center = new Vector3(0, 0, -10);
+            sphere.radius = (float)3;
             List < pixel > framebuffer = new List<pixel>();
 
-            for (int i = 0; i < height; i ++)
+            //for (int i = 0; i < height; i ++)
+            //{
+            //    for (int j = 0; j < width; j++)
+            //    {
+            //        pixel newPixel = new pixel();
+
+            //        newPixel.r = (i* colorDepth) / height;
+            //        newPixel.g = ((width - j)* colorDepth) / width;
+            //        newPixel.b = ((height - i) * colorDepth) / height;
+
+            //        framebuffer.Add(newPixel);
+            //    }
+            //}
+
+            for (int i = 0; i < height; i++)
             {
                 for (int j = 0; j < width; j++)
                 {
                     pixel newPixel = new pixel();
 
-                    newPixel.r = (i* colorDepth) / height;
-                    newPixel.g = ((width - j)* colorDepth) / width;
-                    newPixel.b = ((height - i) * colorDepth) / height;
+                    float x = (float)((2 * (i + 0.5) / (float)width - 1) * Math.Tan(fov / 2) * width / (float)height);
+                    float y = (float)(-(2 * (j + 0.5) / (float)height - 1) * Math.Tan(fov / 2));
+                    
+                    Vector3 dir = Vector3.Normalize(new Vector3(x, y, -1));
+
+                    Vector3 color = cast_ray(new Vector3(0, 0, 0), dir, sphere);
+
+                    newPixel.r = (int)color.X;
+                    newPixel.g = (int)color.Y;
+                    newPixel.b = (int)color.Z;
 
                     framebuffer.Add(newPixel);
                 }
@@ -47,6 +73,19 @@ namespace Raytracing_Csharp
                     outputFile.WriteLine(pxl.r.ToString() + " " + pxl.g.ToString() + " " + pxl.b.ToString());
             }
         }
+
+        static  Vector3 cast_ray(Vector3 orig, Vector3 dir, Sphere sphere)
+        {
+            var color1 = new Vector3(0,0,100); //Blue
+            var color2 = new Vector3(0, 50, 0); //Green
+
+            float sphere_dist = float.MaxValue;
+            if (!sphere.ray_intersect(orig, dir, sphere_dist))
+            {
+                return color1; //background Color
+            }
+            return color2;
+        }
     }
 }
 public struct pixel
@@ -58,19 +97,31 @@ public struct pixel
 
 struct Sphere
 {
-    Vector3 center;
-    float radius;
+    public Vector3 center { get; set; }
+    public float radius { get; set; }
     public Sphere(Vector3 orig, float r)
     {
         this.center = orig;
         this.radius = r;
     }
 
-    bool ray_intersect(Vector3 orig, Vector3 dir, float t)
+    public bool ray_intersect(Vector3 orig, Vector3 dir, double t)
     {
         Vector3 L = center - orig;
-        float tca = L * dir;
-        float d2 = L * L - tca * tca;
+        float tca = L.Length() * dir.Length();
+        float d2 = L.Length() * L.Length() - tca * tca;
+        if (d2 < radius * radius)
+        {
+            return false;
+        }
+        //double thc = Math.Sqrt(radius * radius - d2);
+        //t = tca - thc;
+        //double t1 = tca + thc;
+        //if (t < 0)
+        //{
+        //    t = t1;
+        //    return false;
+        //}
         return true;
     }
 }
